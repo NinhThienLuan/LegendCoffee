0b35a2a859efd45e9da51cc884054f150826ca2c
Ninh Thiện Luân <luanninh2005@gmail.com>
2026-04-14 07:56:06 +0700

    initial commit

---
+++ src/main/java/fpt/legendcoffee/LegendcoffeeApplication.java
@@ -0,0 +1,13 @@
+package fpt.legendcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegendcoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegendcoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fpt/legendcoffee/LegendcoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.legendcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegendcoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
