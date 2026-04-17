220ce62f481f74bb9cb11caab3fab2ddd1595186
Ninh Thiện Luân <luanninh2005@gmail.com>
2026-04-17 08:48:34 +0700

    [update] auth controller for forget and reset password

--- src/main/java/fpt/legendcoffee/common/config/SecurityConstants.java
+++ src/main/java/fpt/legendcoffee/common/config/SecurityConstants.java
@@ -6,16 +6,15 @@ public class SecurityConstants {
             "/home",
             "/login",
             "/register",
+            "/forgot-password",
+            "/reset-password",
             "/css/**",
             "/js/**",
             "/images/**",
-<<<<<<< HEAD
             "/favicon.ico",
             "/payment/vnpay/callback",
-            "/payment/vnpay/ipn"
-=======
+            "/payment/vnpay/ipn",
             "/assets/**",
-            "/favicon.ico"
->>>>>>> 917fd1f7a64e3b4c54ee92351407c36fe9ccc917
+            "/templates/**"
     };
 }
