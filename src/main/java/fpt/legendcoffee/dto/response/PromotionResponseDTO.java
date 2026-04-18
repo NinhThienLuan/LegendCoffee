25eae2295bc19f55e1284a02066713bf23bbb295
dtan812 <tanndse180224@fpt.edu.vn>
2026-04-19 01:24:27 +0700

    feat: CRUD promotion

---
+++ src/main/java/fpt/legendcoffee/dto/response/PromotionResponseDTO.java
@@ -0,0 +1,21 @@
+package fpt.legendcoffee.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PromotionResponseDTO {
+    private Long id;
+    private String type;
+    private BigDecimal value;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
