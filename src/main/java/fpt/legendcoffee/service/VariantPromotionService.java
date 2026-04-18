74fd5d296f32d1ba2c813619063dda01c48fa65c
dtan812 <tanndse180224@fpt.edu.vn>
2026-04-19 01:25:03 +0700

    feat: variant-promotions

---
+++ src/main/java/fpt/legendcoffee/service/VariantPromotionService.java
@@ -0,0 +1,14 @@
+package fpt.legendcoffee.service;
+
+import fpt.legendcoffee.dto.response.PromotionDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface VariantPromotionService {
+//    void assignPromotion(Long promotionId, Long variantId);
+//    void removePromotion(Long promotionId, Long variantId);
+//    List<PromotionDTO> getPromotionsByVariant(Long variantId);
+//    List<ProductVariant> getVariantsByPromotion(Long promotionId);
+}
