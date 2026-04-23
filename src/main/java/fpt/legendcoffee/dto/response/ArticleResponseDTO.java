243a09ceed85a70f3da46ce14620be6b59002943
khanh <khanhle0209c@gmail.com>
2026-04-23 07:20:23 +0700

    [FIX] validate

--- src/main/java/fpt/legendcoffee/dto/response/ArticleResponseDTO.java
+++ src/main/java/fpt/legendcoffee/dto/response/ArticleResponseDTO.java
@@ -1,5 +1,7 @@
 package fpt.legendcoffee.dto.response;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,7 +9,11 @@ import lombok.Data;
 @Builder
 public class ArticleResponseDTO {
     private Long id;
+    @NotNull(message = "Title không thể để trống")
+    @NotBlank(message = "Title không thể chỉ chứa khoảng trắng")
     private String title;
+
+
     private String authorName; // Chỉ lấy tên tác giả thay vì cả Object User
     private String summary;
     private String publishedDate; // Đã format dd/MM/yyyy
